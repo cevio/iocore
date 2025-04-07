4931e45a9ca2e6649aac51a466404009ba6bcfa5
沈赟杰 <shenyunjie@znrank.com>
2025-04-07 13:25:02 +0800

    update

--- packages/micro-ws-agent/src/service.ts
+++ packages/micro-ws-agent/src/service.ts
@@ -1,15 +1,21 @@
 import Component from "@iocore/component";
 import MicroWebSocketAgent from "./index";
-import { Channel } from "@iocore/micro-ws";
+import { Channel, Exception } from "@iocore/micro-ws";
 
 export abstract class Service<T extends any[] = [], R = any> extends Component {
   protected readonly channel: Channel;
   protected readonly agent: MicroWebSocketAgent;
   public abstract exec(...args: T): R | Promise<R>;
-  public connect(host: string) {
-    return this.channel.server.use(host);
-  }
-  public use(namespace: string) {
-    return this.agent.where(namespace);
+
+  public async fetch<R = any>(url: `ws://${string}`, args: any[] = [], timeout?: number) {
+    const uri = new URL(url);
+    if (uri.protocol !== 'ws:') {
+      throw new Exception(461, 'protocol unaccept');
+    }
+    const namespace = uri.host;
+    const router = uri.pathname;
+    const channel = await this.agent.where(namespace);
+    const { response } = channel.fetch(router, args, timeout);
+    return await response<R>();
   }
 }
